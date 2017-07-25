6bfacc9261458a826310dd3bdbb746d25d0442b1
Kateryna Bondareva <kbondareva@gmail.com>
2017-07-24 20:18:47 -0400

    Added webpack and empty files

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack');
+const path = require('path');
+
+
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	entry: './src/app.js',
+
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+
+				options: {
+					presets: ['es2015']
+				}
+			},
+			{
+				test: /\.css$/,
+
+				use: ExtractTextPlugin.extract({
+					use: [
+						{
+							loader: 'css-loader',
+							options: {
+								sourceMap: true
+							}
+						}
+					],
+					fallback: 'style-loader'
+				})
+			}
+		]
+	},
+
+	plugins: [new UglifyJSPlugin(), new ExtractTextPlugin('style.css')]
+};
