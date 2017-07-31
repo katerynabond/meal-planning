99172e1fa10d592717cbddeb2d51dad8ecdd90db
Kateryna Bondareva <kbondareva@gmail.com>
2017-07-30 21:20:02 -0400

    Wiring day navigation

--- src/app.js
+++ src/app.js
@@ -11,12 +11,26 @@ angular.module('meal-planning')
 MainController.$inject = [];
 
 function MainController(){
-  this.message = 'hello from angular';
-  this.previousWeekDay = 'Sunday';
-  this.nextWeekDay = 'Tuesday';
+
+  this.dayNames = {
+    0 : 'Sunday',
+    1 : 'Monday',
+    2 : 'Tuesday',
+    3 : 'Wednesday',
+    4 : 'Thursday',
+    5 : 'Friday',
+    6 : 'Saturday',
+  };
+
+  this.currentDate = new Date();
+  this.previousWeekDay = 'Nothing';
+  this.currentWeekDay = 'Nothing';
+  this.nextWeekDay = 'Nothing';
+  
   this.breakfastSelected = false;
   this.lunchSelected = true;
   this.dinnerSelected = false;
+
   var self = this;
 
   this.selectCourse = function(courseName){
@@ -25,4 +39,30 @@ function MainController(){
     self.dinnerSelected = courseName === 'dinner';
   };
 
+  this.setDate = function(newDate) {
+    self.currentDate = newDate;
+    self.currentWeekDay = self.dayNames[self.currentDate.getDay()];
+    
+    var yesterday = new Date(newDate);
+    yesterday.setDate(yesterday.getDate()-1);
+    self.previousWeekDay = self.dayNames[yesterday.getDay()];
+
+    var tomorrow = new Date(newDate);
+    tomorrow.setDate(tomorrow.getDate()+1);
+    self.nextWeekDay = self.dayNames[tomorrow.getDay()];
+  };
+
+  this.nextDay = function(){
+    var nextDate = new Date(self.currentDate);
+    nextDate.setDate(nextDate.getDate()+1);
+    self.setDate(nextDate);
+  };
+
+  this.previousDay = function(){
+    var prevDate = new Date(self.currentDate);
+    prevDate.setDate(prevDate.getDate()-1);
+    self.setDate(prevDate);
+  };
+
+  this.setDate(new Date());
 }
